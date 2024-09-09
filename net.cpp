7b26280ea4ce5952d817f755434f6886fbf7c7e6
RonB8 <ronbuaron8@gmail.com>
2024-09-09 12:38:08 +0300

    _

--- net.cpp
+++ net.cpp
@@ -12,7 +12,26 @@ using namespace std;
 
 int main() {
 
-    int serverSocket = sock
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INNARRD_ANY;
+
+
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    listen(serverSocket, 5);
+
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+    // const char* msg = "Hi dear client!";
+
+    char buffer[1024] = {0};
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    cout << "msg from client is: " << buffer << endl;
+
+    close(serverSocket);
 
     return 0;
 }
